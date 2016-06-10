f3fac7d1823e464fe80fdc3cccc0e98158ade4b6
Krystan Menne <kmenne@gmail.com>
2016-06-09 19:34:38 -0700

    add information about createServer function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // uses node's built-in http module to act as a server
 var http = require('http');
 http.createServer(function(request, response) {
+  // this callback is executed whenever the server receives a new request
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.write('Hello World');
   response.end();
